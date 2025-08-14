bef3343977fe3713a3ce544245860449800e4660
noaj-koelslaw <153475792+noaj-koelslaw@users.noreply.github.com>
2025-08-14 10:01:39 -0500

    startcof my rust project

---
+++ hello_cargo/target/debug/deps/hello_cargo-0586e22a61b791b4.d
@@ -0,0 +1,5 @@
+/Users/crystalkoelzer/Documents/GitHub/Rust/hello_cargo/target/debug/deps/hello_cargo-0586e22a61b791b4.d: src/main.rs
+
+/Users/crystalkoelzer/Documents/GitHub/Rust/hello_cargo/target/debug/deps/hello_cargo-0586e22a61b791b4: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/crystalkoelzer/Documents/GitHub/Rust/hello_cargo/target/debug/hello_cargo: /Users/crystalkoelzer/Documents/GitHub/Rust/hello_cargo/src/main.rs
